87eb7e4f9b405ecc19756b0e8cfa145f2c2b823f
Esethral <evanabel311@gmail.com>
2026-03-26 20:20:51 -0400

    updated comments

--- app/matches/[id]/page.tsx
+++ app/matches/[id]/page.tsx
@@ -4,7 +4,7 @@ import { CREATOR_BASES, PERSON_IMAGES } from '@/lib/fakeCreators';
 import MatchesReveal, { TileData } from './MatchesReveal';
 
 // This page calls the database to populate all necessary fields and data based on a session ID
-// Then passes it to the browser to immediately render
+// Then passes it to the browser to immediately render the matches page
 export default async function MatchPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   const session = getSession(id);
@@ -12,6 +12,7 @@ export default async function MatchPage({ params }: { params: Promise<{ id: stri
 
   const ratings = session.creator_ratings ?? {};
 
+  // Map through the PERSON_IMAGES (key) to create tiles data, updating it with Andy generated creator details and sorting by match percentage
   const tiles: TileData[] = [...PERSON_IMAGES]
     .map((src) => {
       const base = CREATOR_BASES[src];
